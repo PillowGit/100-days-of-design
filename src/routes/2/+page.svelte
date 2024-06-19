310bd3b4024677fc031cd997b8103df16bf8066d
PillowGit <goldentaco727@gmail.com>
2024-06-19 19:21:27 -0400

    meh i dont wanna do day2

---
+++ src/routes/2/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  
+</script>
+
+  <div class="container">
+    <div class="filler"></div>
+    <div class="card-holder">a</div>
+    <div class="filler">a</div>
+  </div>
+
+<style>
+  
+  .container {
+    width: 80%;
+    aspect-ratio: 1/1.5;
+    max-height: 100vh;
+    display: grid;
+    grid-template-rows: 25% 1fr 25%;
+    border: 1px solid red;
+  }
+  .a {
+    text-align: center;
+    width: 100%;
+    height: 100%;
+    border: 1px solid white;
+    border-radius: 1rem;
+  }
+</style>
